bafa721d41d7356cfde649e027fbc37946c62c81
unknown <nirmalp2@illinois.edu>
2020-11-13 18:07:46 -0500

    Added navigation bar

--- FoodKeepProject/app/src/main/java/com/example/foodkeepproject/MainActivity.java
+++ FoodKeepProject/app/src/main/java/com/example/foodkeepproject/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.foodkeepproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final PantryFragment pantryFragment = new PantryFragment();
+    final GroceryFragment groceryFragment = new GroceryFragment();
+    final SettingsFragment settingsFragment = new SettingsFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    private Fragment active = pantryFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm.beginTransaction().add(R.id.contentFrame, settingsFragment, "settings").hide(settingsFragment).commit();
+        fm.beginTransaction().add(R.id.contentFrame, groceryFragment, "grocery").hide(groceryFragment).commit();
+        fm.beginTransaction().add(R.id.contentFrame, pantryFragment, "pantry").commit();
+
+        BottomNavigationView nav = (BottomNavigationView) findViewById(R.id.navigationBar);
+
+        nav.setOnNavigationItemSelectedListener(
+                item -> {
+                    switch (item.getItemId()) {
+                        case R.id.pantry:
+                            fm.beginTransaction().hide(active).show(pantryFragment).commit();
+                            active = pantryFragment;
+                            break;
+                        case R.id.grocery:
+                            fm.beginTransaction().hide(active).show(groceryFragment).commit();
+                            active = groceryFragment;
+                            break;
+                        case R.id.settings:
+                            fm.beginTransaction().hide(active).show(settingsFragment).commit();
+                            active = settingsFragment;
+                            break;
+                    }
+                    return true;
+                }
+        );
     }
 }
